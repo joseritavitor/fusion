0ee2e93aed5b231f98e9906710acd7bed0da805c
joseritavitor <jrtvitor.cgm@gmail.com>
2021-01-04 22:23:14 -0300

    Arquivos do heroku

--- fusion/settings.py
+++ fusion/settings.py
@@ -17,7 +17,6 @@ import os
 
 import dj_database_url
 
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,5 +156,3 @@ DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
 
 
 LOGOUT_REDIRECT_URL = 'index'
-
-django_heroku.settings(locals())
